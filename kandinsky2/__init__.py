efb72dd450fbc1299931b61cfd5a913ad2f9d286
Yu Zhenchuan <yuadsl3010@gmail.com>
2023-05-14 15:56:35 +0800

    code optimize

--- kandinsky2/__init__.py
+++ kandinsky2/__init__.py
@@ -1,11 +1,12 @@
 import os
-from huggingface_hub import hf_hub_url, cached_download
+from huggingface_hub import hf_hub_url, cached_download, hf_hub_download
 from copy import deepcopy
 from omegaconf.dictconfig import DictConfig
 
 from .configs import CONFIG_2_0, CONFIG_2_1
 from .kandinsky2_model import Kandinsky2
 from .kandinsky2_1_model import Kandinsky2_1
+from typing import Dict, Optional
 
 
 def get_kandinsky2_0(
@@ -92,28 +93,27 @@ def get_kandinsky2_1(
     cache_dir="/tmp/kandinsky2",
     use_auth_token=None,
     use_flash_attention=False,
+    proxies: Optional[Dict] = None,
 ):
+    repo_id="ai-forever/Kandinsky_2.1"
     cache_dir = os.path.join(cache_dir, "2_1")
     config = DictConfig(deepcopy(CONFIG_2_1))
     config["model_config"]["use_flash_attention"] = use_flash_attention
     if task_type == "text2img":
         model_name = "decoder_fp16.ckpt"
-        config_file_url = hf_hub_url(repo_id="sberbank-ai/Kandinsky_2.1", filename=model_name)
     elif task_type == "inpainting":
         model_name = "inpainting_fp16.ckpt"
-        config_file_url = hf_hub_url(repo_id="sberbank-ai/Kandinsky_2.1", filename=model_name)
-    cached_download(
-        config_file_url,
+    hf_hub_download(
+        repo_id=repo_id,
+        filename=model_name,
         cache_dir=cache_dir,
-        force_filename=model_name,
         use_auth_token=use_auth_token,
     )
     prior_name = "prior_fp16.ckpt"
-    config_file_url = hf_hub_url(repo_id="sberbank-ai/Kandinsky_2.1", filename=prior_name)
-    cached_download(
-        config_file_url,
+    hf_hub_download(
+        repo_id=repo_id,
+        filename=prior_name,
         cache_dir=cache_dir,
-        force_filename=prior_name,
         use_auth_token=use_auth_token,
     )
 
@@ -126,27 +126,24 @@ def get_kandinsky2_1(
         "tokenizer.json",
         "tokenizer_config.json",
     ]:
-        config_file_url = hf_hub_url(repo_id="sberbank-ai/Kandinsky_2.1", filename=f"text_encoder/{name}")
-        cached_download(
-            config_file_url,
+        hf_hub_download(
+            repo_id=repo_id,
+            filename=f"text_encoder/{name}",
             cache_dir=cache_dir_text_en,
-            force_filename=name,
             use_auth_token=use_auth_token,
         )
 
-    config_file_url = hf_hub_url(repo_id="sberbank-ai/Kandinsky_2.1", filename="movq_final.ckpt")
-    cached_download(
-        config_file_url,
+    hf_hub_download(
+        repo_id=repo_id,
+        filename="movq_final.ckpt",
         cache_dir=cache_dir,
-        force_filename="movq_final.ckpt",
         use_auth_token=use_auth_token,
     )
 
-    config_file_url = hf_hub_url(repo_id="sberbank-ai/Kandinsky_2.1", filename="ViT-L-14_stats.th")
-    cached_download(
-        config_file_url,
+    hf_hub_download(
+        repo_id=repo_id,
+        filename="ViT-L-14_stats.th",
         cache_dir=cache_dir,
-        force_filename="ViT-L-14_stats.th",
         use_auth_token=use_auth_token,
     )
 
@@ -167,6 +164,7 @@ def get_kandinsky2(
     use_auth_token=None,
     model_version="2.1",
     use_flash_attention=False,
+    proxies: Optional[Dict] = None,
 ):
     if model_version == "2.0":
         model = get_kandinsky2_0(
@@ -182,6 +180,7 @@ def get_kandinsky2(
             cache_dir=cache_dir,
             use_auth_token=use_auth_token,
             use_flash_attention=use_flash_attention,
+            proxies=proxies
         )
     else:
         raise ValueError("Only 2.0 and 2.1 is available")
--- test.py
+++ test.py
@@ -9,6 +9,12 @@ os.environ['HTTPS_PROXY']="127.0.0.1:60119"
 #socks.set_default_proxy(socks.SOCKS5, "127.0.0.1", 60118)
 #socket.socket = socks.socksocket
 
-model = get_kandinsky2('cuda', task_type='text2img')
+model = get_kandinsky2('cuda',
+                       task_type='text2img',
+                       cache_dir='I:\\ai\\cv\\models\\kandinsky2',
+                       proxies={
+                           'http': '127.0.0.1:60119',
+                           'https': '127.0.0.1:60119',
+                        })
 images = model.generate_text2img('A teddy bear на красной площади', batch_size=4, h=512, w=512, num_steps=75, denoised_type='dynamic_threshold', dynamic_threshold_v=99.5, sampler='ddim_sampler', ddim_eta=0.05, guidance_scale=10)
 cv.imwrite('./test.img', images)
